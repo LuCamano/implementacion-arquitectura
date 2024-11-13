abb02abfc04fca67541512063b095437922090a4
Luciano <lu.camano@duocuc.cl>
2024-11-13 00:36:34 -0300

    Agregar vistas y rutas para Edificio, Departamento, Pago, Gasto y Servicio

--- run.py
+++ run.py
@@ -1,6 +1,11 @@
 from app import create_app
+from views import edificio_blueprint, departamento_blueprint, pago_blueprint, gasto_blueprint
 
 app = create_app()
+app.register_blueprint(edificio_blueprint)
+app.register_blueprint(departamento_blueprint)
+app.register_blueprint(pago_blueprint)
+app.register_blueprint(gasto_blueprint)
 
 if __name__ == '__main__':
     app.run(debug=True)
