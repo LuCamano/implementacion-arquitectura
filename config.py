94bb9cbc72c12f855e7aae5967bcf193badeeb8d
Luciano <lu.camano@duocuc.cl>
2024-12-12 04:29:29 -0300

    Actualizar la configuración de la base de datos para usar la variable de entorno DATABASE_URL

--- config.py
+++ config.py
@@ -1,4 +1,6 @@
+import os
+
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
