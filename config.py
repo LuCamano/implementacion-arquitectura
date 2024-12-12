c1b13aa05bb0db22e394c93b67cd27fda2f1f2ae
Luciano <lu.camano@duocuc.cl>
2024-12-12 05:34:37 -0300

    Habilitar el seguimiento de modificaciones en SQLAlchemy

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
     DEBUG = False
