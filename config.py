5dd26f9f75330f555e21c54a6ca48a704549bf43
Luciano <lu.camano@duocuc.cl>
2024-12-12 04:47:55 -0300

    Corregir la construcción de la URI de la base de datos para PostgreSQL en la configuración

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = 'postgresql' + os.getenv('DATABASE_URL')[7]
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL').replace("://", "ql://", 1)
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = False
