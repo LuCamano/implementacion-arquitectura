ac6669cfc26d90ad95bd19ae4b9a1e837c4e7145
Luciano <lu.camano@duocuc.cl>
2024-12-13 17:15:46 -0300

    Registrar nuevos blueprints para Empleado, Persona, Residente y Propietario en la aplicación

--- run.py
+++ run.py
@@ -1,5 +1,5 @@
 from app import create_app
-from views import edificio_blueprint, departamento_blueprint, pago_blueprint, gasto_blueprint, servicio_blueprint
+from views import edificio_blueprint, departamento_blueprint, pago_blueprint, gasto_blueprint, servicio_blueprint, empleado_edificio_blueprint, empleado_blueprint, persona_blueprint, residente_blueprint, propietario_blueprint
 
 app = create_app()
 app.register_blueprint(edificio_blueprint)
@@ -7,6 +7,11 @@ app.register_blueprint(departamento_blueprint)
 app.register_blueprint(pago_blueprint)
 app.register_blueprint(gasto_blueprint)
 app.register_blueprint(servicio_blueprint)
+app.register_blueprint(empleado_edificio_blueprint)
+app.register_blueprint(empleado_blueprint)
+app.register_blueprint(persona_blueprint)
+app.register_blueprint(residente_blueprint)
+app.register_blueprint(propietario_blueprint)
 
 if __name__ == '__main__':
     app.run()
